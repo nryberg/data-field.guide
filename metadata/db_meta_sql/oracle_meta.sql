3154b509f8be778d318b52aab3aee18070b5e482
nryberg <nryberg@gmail.com>
2018-05-15 20:18:26 -0500

    Correcting oracle

--- metadata/db_meta_sql/oracle_meta.sql
+++ metadata/db_meta_sql/oracle_meta.sql
@@ -1,14 +1,15 @@
- SELECT 'xyz' as Database_name,
-        'abc' as Database_server,
-        'DB2' as Database_Type,
-        TBCreator as Schema_Name,
-        TBNAME as Table_Name,
-        NAME as Column_Name,
-        COLNO as Column_Ordinal,
-        COLTYPE as Column_Type,
-        LONGLENGTH as Column_Length,
-        Length as Column_Precision,
-        Scale as Column_scale,
-        CURRENT_DATE as Run_Date
-FROM SYSIBM.SYSCOLUMNS
-ORDER BY TBCreator, TBName, Name
+  SELECT 'xyz' as database_name,
+      'abc' as server,
+      'Oracle' as Database_Type,
+      Owner as schema_owner_name,
+      table_name as table_name,
+      column_name as column_name,
+      column_id as column_ordinal,
+       data_type as Column_type,
+       data_length as Column_length,
+       data_precision as Column_precision,
+       data_scale as Column_scale,
+       Current_Date as Run_Date
+  FROM all_tab_columns
+WHERE OWNER IN ('def', 'ghi')
+ORDER BY Owner, TABLE_NAME, COLUMN_ID;
