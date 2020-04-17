db972fb503d712bf3f224343ab41c35ee219ba9d
Nick Ryberg <nryberg@gmail.com>
2020-04-17 10:54:45 -0500

    Makes SQLITE_Meta match database template.

--- metadata/db_meta_sql/sqlite_meta.sql
+++ metadata/db_meta_sql/sqlite_meta.sql
@@ -1,10 +1,17 @@
-SELECT m.name as tableName, 
-       p.name as columnName,
-       p.type as columnType,
-       p.cid as columnOrdinal,
-       date('now') as timestamp
+SELECT 'Things.sqlite3' as Database_Name,
+       'SQLite' as Database_Type,
+       '(file based)'   as Database_server,
+       NULL       as Schema_Name,
+       m.name as Table_Name, 
+       p.name as Column_Name,
+       p.cid as Column_Ordinal,
+       p.type as Column_Type,
+       NULL as Column_Length,
+        NULL as Column_Precision,
+        NULL AS Column_Scale,
+       date('now') as Run_Date
 FROM sqlite_master m
 left outer join pragma_table_info((m.name)) p
      on m.name <> p.name
-order by tableName, p.cid, columnName
+order by Table_Name, Column_Ordinal, Column_Name
 ;
