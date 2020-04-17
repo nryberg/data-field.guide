293efef6ce4f84b413b35c3b7633fbd2112ce661
Nick Ryberg <nryberg@gmail.com>
2020-04-17 10:47:16 -0500

    Updates SQLITE meta

--- metadata/db_meta_sql/sqlite_meta.sql
+++ metadata/db_meta_sql/sqlite_meta.sql
@@ -1,7 +1,8 @@
 SELECT m.name as tableName, 
        p.name as columnName,
        p.type as columnType,
-       p.cid as columnOrdinal
+       p.cid as columnOrdinal,
+       date('now') as timestamp
 FROM sqlite_master m
 left outer join pragma_table_info((m.name)) p
      on m.name <> p.name
